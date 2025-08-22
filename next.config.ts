b52fd40f7fdb0e18ec3675e9e1e131ee48099728
Sayan Mondal <sm187966@gmail.com>
2025-08-22 05:43:17 +0000

    "Error: Invalid src prop (https://i.postimg.cc/0NKPssn6/weekly.png) on `

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'drive.google.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+      },
     ],
   },
 };
